97181c49898fd90aeb51423214c9edb25fa1da77
Filiprogrammer <filip@mg6.at>
2020-03-16 23:00:41 +0100

    Change text color of test fail message to red

--- kernel/src-test/test.c
+++ kernel/src-test/test.c
@@ -4,6 +4,7 @@
 bool test_failed = false;
 
 void test_fail(const char* file, uint32_t line, const char* function) {
+    settextcolor(4, 0);
     printf("%s:%u:%s:FAIL:", file, line, function);
     test_failed = true;
 }
