56d447f5906256978d797165e5b6a700c402660a
Filiprogrammer <filip@mg6.at>
2020-10-07 21:05:43 +0200

    Fix tools directory being read only after download

--- download_tools.sh
+++ download_tools.sh
@@ -5,5 +5,5 @@ cd "$(dirname "$(readlink -f "$0")")"
 if [ -z "$(ls -A tools 2>/dev/null)" ]; then
     wget http://filip.mg6.at/chaostools.zip
     unzip chaostools.zip -d tools && rm chaostools.zip
-    chmod -R 555 tools
+    chmod -R 777 tools
 fi
