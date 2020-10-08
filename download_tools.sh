8108376f0bdaae224246cd124c562e5abcb21818
Filiprogrammer <filip@mg6.at>
2020-10-09 00:02:47 +0200

    Split up Windows & Linux tool downloads

--- download_tools.sh
+++ download_tools.sh
@@ -2,8 +2,8 @@
 
 cd "$(dirname "$(readlink -f "$0")")"
 
-if [ -z "$(ls -A tools 2>/dev/null)" ]; then
-    wget http://filip.mg6.at/chaostools.zip
-    unzip chaostools.zip -d tools && rm chaostools.zip
+if [ -z "$(ls -A tools/qemu-linux 2>/dev/null)" ]; then
+    wget http://filip.mg6.at/chaostools-linux.zip
+    unzip -o chaostools-linux.zip -d tools && rm chaostools-linux.zip
     chmod -R 777 tools
 fi
